1e8406834bdd1ff88a5228bc982863a87aeeb42e
Dom Tiapkov <tiapkov.dom@gmail.com>
2026-05-10 00:18:45 -0400

    Added: GoalController and get all method.

---
+++ src/main/java/com/example/springbootapi/controller/GoalController.java
@@ -0,0 +1,26 @@
+package com.example.springbootapi.controller;
+
+import com.example.springbootapi.dto.goal.GoalResponseDTO;
+import com.example.springbootapi.services.GoalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/goals")
+@RequiredArgsConstructor
+public class GoalController {
+    private final GoalService goalService;
+
+    @GetMapping
+    public ResponseEntity<Page<GoalResponseDTO>> getAllGoals(@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        Page<GoalResponseDTO> goals = goalService.getAllGoals(pageable);
+        return ResponseEntity.ok(goals);
+    }
+}
