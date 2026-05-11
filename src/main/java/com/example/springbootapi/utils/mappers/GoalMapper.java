77f7900f3349097a514ac58ed9d054221a576c37
Dom Tiapkov <tiapkov.dom@gmail.com>
2026-05-10 22:48:28 -0400

    Added: AfterMapping for the custom method to set goal.

--- src/main/java/com/example/springbootapi/utils/mappers/GoalMapper.java
+++ src/main/java/com/example/springbootapi/utils/mappers/GoalMapper.java
@@ -3,14 +3,22 @@ package com.example.springbootapi.utils.mappers;
 import com.example.springbootapi.dto.goal.GoalRequestDTO;
 import com.example.springbootapi.dto.goal.GoalResponseDTO;
 import com.example.springbootapi.model.Goal;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {InitiativeMapper.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface GoalMapper {
     GoalResponseDTO toGoalResponseDTO(Goal goal);
     Goal toGoal(GoalResponseDTO goalResponseDTO);
     Goal toGoal(GoalRequestDTO goalRequestDTO);
     GoalRequestDTO toGoalRequestDTO(Goal goal);
     void updateGoalFromDTO(GoalRequestDTO goalRequestDTO, @MappingTarget Goal goal);
+    @AfterMapping
+    default void linkInitiatives(@MappingTarget Goal goal) {
+        if (goal.getInitiatives() != null) {
+            goal.getInitiatives().forEach(initiative -> initiative.setGoal(goal));
+        }
+    }
 }
