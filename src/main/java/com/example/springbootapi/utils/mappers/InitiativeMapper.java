1954da67182b1962ec53809849826b4529a348d6
Dom Tiapkov <tiapkov.dom@gmail.com>
2026-05-10 22:49:55 -0400

    Added: Mapping source and target for Initiatives

--- src/main/java/com/example/springbootapi/utils/mappers/InitiativeMapper.java
+++ src/main/java/com/example/springbootapi/utils/mappers/InitiativeMapper.java
@@ -5,13 +5,17 @@ import com.example.springbootapi.dto.initiative.InitiativeResponseDTO;
 import com.example.springbootapi.model.Initiative;
 import jakarta.validation.Valid;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface InitiativeMapper {
-    InitiativeResponseDTO toInitiativeResponseDTO(Object initiative);
+    @Mapping(source = "goal.id", target = "goalId")
+    InitiativeResponseDTO toInitiativeResponseDTO(Initiative initiative);
     Initiative toInitiative(InitiativeResponseDTO initiativeResponseDTO);
     Initiative toInitiative(InitiativeRequestDTO initiativeRequestDTO);
+    InitiativeRequestDTO toInitiativeRequestDTO(Initiative initiative);
 
     void updateInitiativeFromDTO(InitiativeRequestDTO requestDTO, @MappingTarget Initiative initiative);
 }
