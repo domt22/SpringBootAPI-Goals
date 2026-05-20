1be1348b353fe07e199a3ed882d5b85889a8598a
Dom Tiapkov <tiapkov.dom@gmail.com>
2026-05-19 23:56:09 -0400

    Added: Optional Fields in GoalMapper for GoalRequestDTO.

--- src/main/java/com/example/springbootapi/utils/mappers/GoalMapper.java
+++ src/main/java/com/example/springbootapi/utils/mappers/GoalMapper.java
@@ -3,15 +3,21 @@ package com.example.springbootapi.utils.mappers;
 import com.example.springbootapi.dto.goal.GoalRequestDTO;
 import com.example.springbootapi.dto.goal.GoalResponseDTO;
 import com.example.springbootapi.model.Goal;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", uses = {InitiativeMapper.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(
+        componentModel = "spring",
+        uses = {InitiativeMapper.class},
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface GoalMapper {
     GoalResponseDTO toGoalResponseDTO(Goal goal);
     Goal toGoal(GoalResponseDTO goalResponseDTO);
+    @Mapping(target = "status", defaultValue = "NOT_STARTED")
+    @Mapping(target = "progress", defaultValue = "0")
+    @Mapping(target = "initiatives", defaultExpression = "java(new ArrayList<>())")
     Goal toGoal(GoalRequestDTO goalRequestDTO);
     GoalRequestDTO toGoalRequestDTO(Goal goal);
     void updateGoalFromDTO(GoalRequestDTO goalRequestDTO, @MappingTarget Goal goal);
